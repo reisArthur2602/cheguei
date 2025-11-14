04329f235a430ed613a6ce1a6c17fbd34dcaaa95
@dev.arthur <contato.arthurreis222@gmail.com>
2025-11-14 02:58:24 -0300

    ,

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://10.1.1.243:3333';
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
     baseURL,
-    headers: {
-        'ngrok-skip-browser-warning': 'true',
-    },
 });
